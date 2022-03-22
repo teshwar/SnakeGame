c2f13c058ec2a130f34a20617d0210d98bae1100
teshwar <kttara.chan@gmail.com>
2022-03-21 22:07:39 -0400

    Snake now eats food, but not grow longer

--- main.py
+++ main.py
@@ -24,38 +24,37 @@ class Snake(pygame.sprite.Sprite):
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_LEFT]:
-            self.x_pos = -1
+            self.x_pos = -10
             self.y_pos = 0
             self.snake_head = self.snake_head_left
         
         elif keys[pygame.K_RIGHT]:
-            self.x_pos = 1
+            self.x_pos = 10
             self.y_pos = 0
             self.snake_head = self.snake_head_right
         
         elif keys[pygame.K_UP]:
-           self.y_pos = -1
+           self.y_pos = -10
            self.x_pos = 0
            self.snake_head = self.snake_head_up
         
         elif keys[pygame.K_DOWN]:
-            self.y_pos = 1
+            self.y_pos = 10
             self.x_pos = 0
             self.snake_head = self.snake_head_down
 
-    def snake_move(self):
-        
+    def snake_move(self):    
         self.rect.x += self.x_pos
-        if self.rect.x > 320: 
+        if self.rect.x > 318: 
             self.rect.x = 0
         elif self.rect.x < 0:
-            self.rect.x = 320
+            self.rect.x = 319
 
         self.rect.y += self.y_pos
-        if self.rect.y > 320:
+        if self.rect.y > 310:
             self.rect.y = 0
-        elif self.rect.y < 0:
-            self.rect.y = 320
+        elif self.rect.y < 1:
+            self.rect.y = 319
 
         self.image = self.snake_head
 
@@ -71,13 +70,24 @@ class Snake(pygame.sprite.Sprite):
 #check movement 
 #check eating food or snail tail
 
-class Food():
-    food = pygame.image.load('Resources/Food/food.png')
+class Food(pygame.sprite.Sprite):
+    def __init__(self):
+        super().__init__()
+        food = pygame.image.load('Resources/Food/food.png')
 
-    pass
+        self.image = food
+        self.rect = self.image.get_rect(midbottom = (randint(0,310), randint(0,310)))
+        #(randint(0,320), randint(0,320)
 #check if eaten and random spawn
 
-#screen
+def collision_cake_eaten():
+    if pygame.sprite.spritecollide(snake.sprite, food_group, True):
+        return True
+    else: 
+        return False
+#screen + score
+score = 0
+g_fps = 8
 pygame.init()
 screen = pygame.display.set_mode((320,320))
 pygame.display.set_caption('Snake Game')
@@ -89,14 +99,30 @@ clock = pygame.time.Clock()
 snake = pygame.sprite.GroupSingle()
 snake.add(Snake())
 
+food_group = pygame.sprite.Group()
+food_group.add(Food())
+
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
+            print(f'Your score was :{score}')
             exit()
     screen.fill((0,0,0))
+
+    if collision_cake_eaten():
+        food_group.add(Food())
+        score += 1
+        if (score % 5 == 0):
+            g_fps += 2
+
+    food_group.draw(screen)
+    #food.update()
+
     snake.draw(screen)
     snake.update()
 
+
+
     pygame.display.update()
-    clock.tick(60)
+    clock.tick(g_fps)
